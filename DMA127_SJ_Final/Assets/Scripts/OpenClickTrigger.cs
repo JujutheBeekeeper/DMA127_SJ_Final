55531ec4cca4068d09b5ce338fc6709fd3834fea
Sofia <155522110+eh-human@users.noreply.github.com>
2025-11-30 22:52:46 -0800

    Test

--- DMA127_SJ_Final/Assets/Scripts/OpenClickTrigger.cs
+++ DMA127_SJ_Final/Assets/Scripts/OpenClickTrigger.cs
@@ -2,46 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OpenClickTrigger : MonoBehaviour, IInteractable
+public class OpenClickTrigger : MonoBehaviour
 {
-    [Header("Animation Settings")]
-    [SerializeField] private Animator animator;
-    [SerializeField] private string triggerName = "Interact";
+    LayerMask layerMask;
 
-
-    public void Interact()
+    void Awake()
     {
-        if (animator != null && !string.IsNullOrEmpty(triggerName))
+        layerMask = LayerMask.GetMask("Wall", "Character");
+    }
+
+    // See Order of Execution for Event Functions for information on FixedUpdate() and Update() related to physics queries
+    void FixedUpdate()
+    {
+
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+
         {
-            animator.SetTrigger(triggerName);
-            Debug.Log($"{gameObject.name} animator triggered: {triggerName}");
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
         }
         else
         {
-            Debug.LogWarning($"{gameObject.name} has no animator or trigger set.");
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            Debug.Log("Did not Hit");
         }
+
     }
-    public bool IsAvailable()
-    {
-        return true;
-    }
-
-    //void FixedUpdate()
-    //{
-
-    //    RaycastHit hit;
-    //    // Does the ray intersect any objects excluding the player layer
-    //    if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
-
-    //    {
-    //        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-    //        Debug.Log("Did Hit");
-    //    }
-    //    else
-    //    {
-    //        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
-    //        Debug.Log("Did not Hit");
-    //    }
-
-    //}
 }
